789c3dba14da6575a558e2f4c61f049b0551d046
Shaughnessy <shaughnt@email.sc.edu>
2024-07-12 17:50:34 -0400

    Rough Draft Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
-import { Navbar } from './containers';
+import './App.css';
+import { Navbar, Header, Join, About, Meetings, Events, Team, Contact, Footer } from './containers';
 
 const App = () => {
   return (
     <div className='App'>
         <Navbar />
+        <Header />
+        <Join />
+        <About />
+        <Meetings />
+        <Events />
+        <Team />
+        <Contact />
+        <Footer />
     </div>
   )
 }
--- src/containers/navbar/Navbar.jsx
+++ src/containers/navbar/Navbar.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.png';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+import './Navbar.css';
+
+const Navbar = () => { 
+    const [toggleMenu, setToggleMenu] = useState(false);
+    return (
+        <nav className='acm__navbar'>
+            <div className='acm__navbar-top'>
+                <a href="#home" className='acm__navbar-logo'>
+                    <img src={logo} alt="logo" />
+                </a>
+                <div>
+                    <ul className='acm__navbar-links'>
+                        <li><a href='#Join'>Join</a></li>
+                        <li><a href='#About'>About</a></li>
+                        <li><a href='#Team'>Team</a></li>
+                        <li><a href='#Contact'>Contact</a></li>
+                    </ul>
+                </div>
+                <div className='acm__navbar-menu_open-icon'>
+                    {!toggleMenu && 
+                    <RiMenu3Line color="black" size={27} onClick={() => setToggleMenu(true)} />
+                    }
+                </div>
+            </div>
+            {toggleMenu &&
+            <div className='acm__navbar-menu_drop-down'>
+                <div><a href='#Join'>Join</a></div>
+                <div><a href='#About'>About</a></div>
+                <div><a href='#Team'>Team</a></div>
+                <div><a href='#Contact'>Contact</a></div>
+                <div className='acm__navbar-menu_close-container' onClick={() => setToggleMenu(false)}><RiCloseLine color="white" size={45}/></div>
+            </div>
+            }
+        </nav>
+    )
 }
 
 export default Navbar;
